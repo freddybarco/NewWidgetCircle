f7d760a9dec5c635b5f170d9946b00157dcf6f8f
freddy barco <freddybarco@gmail.com>
2017-07-24 19:55:25 -0500

    cambio aun no probado

--- app/src/main/java/connect/freddybarco/webostv/newwidgetcircle/CircularSlider.java
+++ app/src/main/java/connect/freddybarco/webostv/newwidgetcircle/CircularSlider.java
@@ -1,10 +1,5 @@
 package connect.freddybarco.webostv.newwidgetcircle;
 
-/**
- * Created by Freddy on 07/24/2017.
- */
-
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
@@ -12,33 +7,19 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.SweepGradient;
 import android.graphics.drawable.Drawable;
-import android.os.Build;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
-import java.io.Console;
-import java.util.Arrays;
-
 import static android.content.ContentValues.TAG;
 
+/**
+ * Created by Freddy Barco on 7/24/2017.
+ */
+
 public class CircularSlider extends View {
-
-    /**
-     * Listener interface used to detect when slider moves around.
-     */
-    @SuppressWarnings("WeakerAccess")
-    public interface OnSliderMovedListener {
-
-        /**
-         * This method is invoked when slider moves, providing position of the slider thumb.
-         *
-         * @param pos Value between 0 and 1 representing the current angle.<br>
-         *            {@code pos = (Angle - StartingAngle) / (2 * Pi)}
-         */
-        void onSliderMoved(double pos);
-    }
 
     private int mThumbX;
     private int mThumbY;
@@ -50,7 +31,7 @@ public class CircularSlider extends View {
     private int mCircleRadius;
 
     private Drawable mThumbImage;
-    private int mPadding;
+
     private int mThumbSize;
     private int mThumbColor;
     private int mBorderColor;
@@ -62,7 +43,8 @@ public class CircularSlider extends View {
 
     private Paint mPaint = new Paint();
     private SweepGradient mGradientShader;
-    private OnSliderMovedListener mListener;
+
+
 
     public CircularSlider(Context context) {
         this(context, null);
@@ -72,15 +54,9 @@ public class CircularSlider extends View {
         this(context, attrs, 0);
     }
 
-    public CircularSlider(Context context, AttributeSet attrs, int defStyleAttr) {
+    public CircularSlider(Context context,  AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context, attrs, defStyleAttr);
-    }
 
-    @SuppressWarnings("unused")
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    public CircularSlider(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
         init(context, attrs, defStyleAttr);
     }
 
@@ -89,8 +65,8 @@ public class CircularSlider extends View {
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CircularSlider, defStyleAttr, 0);
 
         // read all available attributes
-        float startAngle = a.getFloat(R.styleable.CircularSlider_start_angle, (float) Math.PI*2);
-        float angle = a.getFloat(R.styleable.CircularSlider_angle, (float) Math.PI *2);
+        float startAngle = a.getFloat(R.styleable.CircularSlider_start_angle, (float) Math.PI * 2);
+        float angle = a.getFloat(R.styleable.CircularSlider_angle, (float) Math.PI * 2);
         int thumbSize = a.getDimensionPixelSize(R.styleable.CircularSlider_thumb_size, 30);
         int thumbColor = a.getColor(R.styleable.CircularSlider_thumb_color, Color.BLACK);
         int borderThickness = a.getDimensionPixelSize(R.styleable.CircularSlider_border_thickness, 20);
@@ -98,84 +74,59 @@ public class CircularSlider extends View {
         String borderGradientColors = a.getString(R.styleable.CircularSlider_border_gradient_colors);
         Drawable thumbImage = a.getDrawable(R.styleable.CircularSlider_thumb_image);
 
-        // save those to fields (really, do we need setters here..?)
-        setStartAngle(startAngle);
-        setAngle(angle);
-        setBorderThickness(borderThickness);
-        setBorderColor(borderColor);
-        if (borderGradientColors != null) {
-            setBorderGradientColors(borderGradientColors.split(";"));
-        }
-        setThumbSize(thumbSize);
-        setThumbImage(thumbImage);
-        setThumbColor(thumbColor);
 
-        // assign padding - check for version because of RTL layout compatibility
-        int padding;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-            int all = getPaddingLeft() + getPaddingRight() + getPaddingBottom() + getPaddingTop() + getPaddingEnd() + getPaddingStart();
-            padding = all / 6;
-        } else {
-            padding = (getPaddingLeft() + getPaddingRight() + getPaddingBottom() + getPaddingTop()) / 4;
+        setmStartAngle(startAngle);
+        setmAngle(angle);
+        setmBorderThickness(borderThickness);
+        setmBorderColor(borderColor);
+        if (borderGradientColors != null) {
+            setmBorderGradientColors(borderGradientColors.split(";"));
         }
-        setPadding(padding);
+        setmThumbSize(thumbSize);
+        setmThumbImage(thumbImage);
+        setmThumbColor(thumbColor);
 
         a.recycle();
     }
 
-    /* ***** Setters ***** */
-    public void setStartAngle(double startAngle) {
-        mStartAngle = startAngle;
+    public void setmThumbImage(Drawable ThumbImage) {
+        mThumbImage = ThumbImage;
     }
 
-    public void setAngle(double angle) {
-        mAngle = angle;
+
+    public void setmThumbSize(int ThumbSize) {
+        mThumbSize = ThumbSize;
     }
 
-    public void setThumbSize(int thumbSize) {
-        mThumbSize = thumbSize;
+    public void setmThumbColor(int ThumbColor) {
+        mThumbColor = ThumbColor;
     }
 
-    public void setBorderThickness(int circleBorderThickness) {
-        mBorderThickness = circleBorderThickness;
+    public void setmBorderColor(int BorderColor) {
+        mBorderColor = BorderColor;
     }
 
-    public void setBorderColor(int color) {
-        mBorderColor = color;
-    }
-
-    public void setBorderGradientColors(String[] colors) {
-        mBorderGradientColors = new int[colors.length];
-        for (int i = 0; i < colors.length; i++) {
-            mBorderGradientColors[i] = Color.parseColor(colors[i]);
+    public void setmBorderGradientColors(String[] BorderGradientColors) {
+        mBorderGradientColors = new int[BorderGradientColors.length];
+        for (int i = 0; i < BorderGradientColors.length; i++) {
+            mBorderGradientColors[i] = Color.parseColor(BorderGradientColors[i]);
         }
     }
 
-    @SuppressWarnings("unused")
-    public void setBorderGradientColors(int[] colors) {
-        if (colors == null) {
-            mBorderGradientColors = null;
-            mGradientShader = null;
-        } else {
-            mBorderGradientColors = Arrays.copyOf(colors, colors.length);
-            mGradientShader = new SweepGradient(mCircleRadius, mCircleRadius, mBorderGradientColors, null);
-        }
+    public void setmBorderThickness(int BorderThickness) {
+        mBorderThickness = BorderThickness;
     }
 
-    public void setThumbImage(Drawable drawable) {
-        mThumbImage = drawable;
+    public void setmStartAngle(double StartAngle) {
+        mStartAngle = StartAngle;
     }
 
-    public void setThumbColor(int color) {
-        mThumbColor = color;
-    }
-
-    public void setPadding(int padding) {
-        mPadding = padding;
+    public void setmAngle(double Angle) {
+        mAngle = Angle;
     }
 
     @Override
-    protected void onSizeChanged(int w, int h, int oldW, int oldH) {
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         // use smaller dimension for calculations (depends on parent size)
         int smallerDim = w > h ? h : w;
 
@@ -188,14 +139,13 @@ public class CircularSlider extends View {
         // save circle coordinates and radius in fields
         mCircleCenterX = largestCenteredSquareRight / 2 + (w - largestCenteredSquareRight) / 2;
         mCircleCenterY = largestCenteredSquareBottom / 2 + (h - largestCenteredSquareBottom) / 2;
-        mCircleRadius = smallerDim / 2 - mBorderThickness / 2 - mPadding;
+        mCircleRadius = smallerDim / 2 - 40;
 
         if (mBorderGradientColors != null) {
             mGradientShader = new SweepGradient(mCircleRadius, mCircleRadius, mBorderGradientColors, null);
         }
 
-        // works well for now, should we call something else here?
-        super.onSizeChanged(w, h, oldW, oldH);
+        super.onSizeChanged(w, h, oldw, oldh);
     }
 
     @Override
@@ -210,8 +160,8 @@ public class CircularSlider extends View {
         if (mGradientShader != null) {
             mPaint.setShader(mGradientShader);
         }
-        canvas.drawCircle(mCircleCenterX, mCircleCenterY, mCircleRadius-20, mPaint);
-        canvas.drawCircle(mCircleCenterX, mCircleCenterY, mCircleRadius+20, mPaint);
+        canvas.drawCircle(mCircleCenterX, mCircleCenterY, mCircleRadius - 20, mPaint);
+        canvas.drawCircle(mCircleCenterX, mCircleCenterY, mCircleRadius + 20, mPaint);
 
         // find thumb position
         mThumbX = (int) (mCircleCenterX + mCircleRadius * Math.cos(mAngle));
@@ -228,73 +178,21 @@ public class CircularSlider extends View {
             canvas.drawCircle(mThumbX, mThumbY, mThumbSize, mPaint);
         }
 
-        int xPos = (canvas.getWidth() / 2);
-        int yPos = (int) ((canvas.getHeight() / 2) - ((mPaint.descent() + mPaint.ascent()) / 2)) ;
+
 
         mPaint.setColor(Color.BLACK);
         mPaint.setTextAlign(Paint.Align.CENTER);
         mPaint.setTextSize(50);
-        canvas.drawText(Integer.toString(value), xPos, yPos, mPaint);
-    }
-
-    /**
-     * Invoked when slider starts moving or is currently moving. This method calculates and sets position and angle of the thumb.
-     *
-     * @param touchX Where is the touch identifier now on X axis
-     * @param touchY Where is the touch identifier now on Y axis
-     */
-    private void updateSliderState(int touchX, int touchY) {
-        int distanceX = touchX - mCircleCenterX;
-        int distanceY = mCircleCenterY - touchY;
-        //noinspection SuspiciousNameCombination
-        double c = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
-        mAngle = Math.acos(distanceX / c);
-        value = (int)((mAngle/(Math.PI))*200);
-
-        //Log.d(TAG,"Angle : "+ mAngle);
-
-        if (distanceY < 0) {
-            mAngle = -mAngle;
-        }
-
-        if (mListener != null) {
-            // notify slider moved listener of the new position which should be in [0..1] range
-            mListener.onSliderMoved((mAngle - mStartAngle) / (2 * Math.PI));
-        }
-    }
-
-    /**
-     * Position setter. This method should be used to manually position the slider thumb.<br>
-     * Note that counterclockwise {@link #mStartAngle} is used to determine the initial thumb position.
-     *
-     * @param pos Value between 0 and 1 used to calculate the angle. {@code Angle = StartingAngle + pos * 2 * Pi}<br>
-     *            Note that angle will not be updated if the position parameter is not in the valid range [0..1]
-     */
-    @SuppressWarnings("unused")
-    public void setPosition(double pos) {
-        if (pos >= 0 && pos <= 1) {
-            mAngle = mStartAngle + pos * 2 * Math.PI;
-        }
-    }
-
-    /**
-     * Saves a new slider moved listener. Set {@link CircularSlider.OnSliderMovedListener} to {@code null} to remove it.
-     *
-     * @param listener Instance of the slider moved listener, or null when removing it
-     */
-    @SuppressWarnings("unused")
-    public void setOnSliderMovedListener(OnSliderMovedListener listener) {
-        mListener = listener;
+        canvas.drawText(Integer.toString(value), mCircleCenterX, mCircleCenterY, mPaint);
     }
 
     @Override
-    @SuppressWarnings("NullableProblems")
-    public boolean onTouchEvent(MotionEvent ev) {
-        switch (ev.getAction()) {
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN: {
                 // start moving the thumb (this is the first touch)
-                int x = (int) ev.getX();
-                int y = (int) ev.getY();
+                int x = (int) event.getX();
+                int y = (int) event.getY();
                 if (x < mThumbX + mThumbSize && x > mThumbX - mThumbSize && y < mThumbY + mThumbSize && y > mThumbY - mThumbSize) {
                     getParent().requestDisallowInterceptTouchEvent(true);
                     mIsThumbSelected = true;
@@ -306,8 +204,8 @@ public class CircularSlider extends View {
             case MotionEvent.ACTION_MOVE: {
                 // still moving the thumb (this is not the first touch)
                 if (mIsThumbSelected) {
-                    int x = (int) ev.getX();
-                    int y = (int) ev.getY();
+                    int x = (int) event.getX();
+                    int y = (int) event.getY();
                     updateSliderState(x, y);
                 }
                 break;
@@ -319,11 +217,36 @@ public class CircularSlider extends View {
                 mIsThumbSelected = false;
                 break;
             }
-        }
 
-        // redraw the whole component
+        }
         invalidate();
         return true;
     }
+
+
+
+    private void updateSliderState(int touchX, int touchY) {
+        int distanceX = touchX - mCircleCenterX;
+        int distanceY = mCircleCenterY - touchY;
+        //noinspection SuspiciousNameCombination
+        double c = Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2));
+
+
+        double brng = Math.atan2(distanceY, distanceX);
+
+        brng = Math.toDegrees(brng);
+        brng = (brng + 360) % 360;
+        brng = 360 - brng; // count degrees counter-clockwise - remove to make clockwise
+        mAngle = Math.acos(distanceX / c);
+        value = (int)((brng/(18))*5);
+
+        //Log.d(TAG,"Angle : "+ brng);
+
+        if (distanceY < 0) {
+            mAngle = -mAngle;
+        }
+
+    }
+
 
 }
